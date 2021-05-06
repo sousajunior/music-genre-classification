f09499b99e69bc9efd9d111cee908a104514802d
sousajunior <cdesousajunior@gmail.com>
2021-05-06 00:24:52 -0300

    Improve extract algorithm

--- extract-mfcc.py
+++ extract-mfcc.py
@@ -5,81 +5,103 @@ import librosa
 
 # directory with all musics tracks
 DATASET_PATH = "genres"
-# file name that will be created with extracted attributes
-JSON_PATH = "extracted_data.json"
+
 # default sample rate of GTZAN tracks
 SAMPLE_RATE = 22050
-# track duration in seconds
-TRACK_DURATION = 3
-# TODO: explain this calc
-SAMPLES_PER_TRACK = SAMPLE_RATE * TRACK_DURATION
 
+# Testar:
+#   Alterar o num_segments para 10 ou algo > que 5
 
-def save_mfcc(dataset_path, json_path, num_mfcc=13, n_fft=1024, hop_length=512, num_segments=5):
+def create_mfcc_features_file(dataset_path, num_mfcc=13, n_fft=1024, hop_length=512, num_segments=5, track_duration=30):
     """Extracts MFCCs from music dataset and saves them into a json file along witgh genre labels.
         :param dataset_path (str): Path to dataset
-        :param json_path (str): Path to json file used to save MFCCs
         :param num_mfcc (int): Number of coefficients to extract
         :param n_fft (int): Interval we consider to apply FFT. Measured in # of samples
         :param hop_length (int): Sliding window for FFT. Measured in # of samples
         :param: num_segments (int): Number of segments we want to divide sample tracks into
-        :return:
+        :param: track_duration (int): Number of track duration in seconds
         """
 
-    print("Extração dos atributos inicializada...")
+    print("Extração das características mfcc inicializada...")
 
-    # dictionary to store mapping, labels, and MFCCs
+    # estrutura de dados para guardar o mapping, labels e MFCCs
     data = {
         "mapping": [],
         "labels": [],
         "mfcc": []
     }
 
-    samples_per_segment = int(SAMPLES_PER_TRACK / num_segments)
-    num_mfcc_vectors_per_segment = math.ceil(samples_per_segment / hop_length)
+    samples_per_track = SAMPLE_RATE * track_duration
+    samples_per_segment = int(samples_per_track / num_segments)
+    expected_num_mfcc_vectors_per_segment = math.ceil(
+        samples_per_segment / hop_length)
 
-    # loop through all genre sub-folder
-    for i, (dirpath, dirnames, filenames) in enumerate(os.walk(dataset_path)):
+    # itera sobre todas as sub pastas de gênero
+    for label_index, (dirpath, _, filenames) in enumerate(os.walk(dataset_path)):
 
-        # ensure we're processing a genre sub-folder level
+        # garante que estamos processando uma sub pasta no nível de gênero
         if dirpath is not dataset_path:
 
-            # save genre label (i.e., sub-folder name) in the mapping
+            # genre/blues => ["genre", "blues"]
             semantic_label = dirpath.split("/")[-1]
+            # salva o nome do gênero na estrutura de mapping
             data["mapping"].append(semantic_label)
-            print("\nProcessing: {}".format(semantic_label))
+            print("\nProcessando: {}".format(semantic_label))
 
-            # process all audio files in genre sub-dir
-            for f in filenames:
+            # processa todos os arquivos de audio no diretório de gênero
+            for filename in filenames:
 
-                # load audio file
-                file_path = os.path.join(dirpath, f)
+                # carrega o arquivo de audio
+                file_path = os.path.join(dirpath, filename)
                 signal, sample_rate = librosa.load(file_path, sr=SAMPLE_RATE)
 
-                # process all segments of audio file
-                for d in range(num_segments):
+                # processa todos os segmentos dos arquivos de audio
+                for segment_index in range(num_segments):
 
-                    # calculate start and finish sample for current segment
-                    start = samples_per_segment * d
-                    finish = start + samples_per_segment
+                    # calcula o inicio e o fim da amostra do segmento atual
+                    start_sample_index = samples_per_segment * segment_index
+                    finish_sample_index = start_sample_index + samples_per_segment
 
-                    # extract mfcc
+                    # extrai o mfcc
                     mfcc = librosa.feature.mfcc(
-                        signal[start:finish], sample_rate, n_mfcc=num_mfcc, n_fft=n_fft, hop_length=hop_length)
+                        signal[start_sample_index:finish_sample_index],
+                        sample_rate,
+                        n_mfcc=num_mfcc,
+                        n_fft=n_fft,
+                        hop_length=hop_length
+                    )
                     mfcc = mfcc.T
 
-                    # store only mfcc feature with expected number of vectors
-                    if len(mfcc) == num_mfcc_vectors_per_segment:
+                    # guarda apenas as características do mfcc que possui o numero esperado de vetores,
+                    # pois se um mfcc não possuir a quantidade correta esperada, ele pode estragar os dados existentes
+                    if len(mfcc) == expected_num_mfcc_vectors_per_segment:
                         data["mfcc"].append(mfcc.tolist())
-                        data["labels"].append(i-1)
-                        print("{}, segment:{}".format(file_path, d+1))
+                        data["labels"].append(label_index-1)
+                        print("{}, segmento:{}".format(
+                            file_path, segment_index+1))
 
-    # save MFCCs to json file
+
+    # nome do arquivo a ser criado para guardar as características extraídas
+    json_path = f"extracted_data_with_{num_segments}_segments_and_{track_duration}_sec.json"
+
+    print("\nSalvando os dados como: {}".format(json_path))
+
+    # salva os MFCCs em um arquivo .json
     with open(json_path, "w") as fp:
         json.dump(data, fp, indent=4)
 
-    print("Extração dos atributos finalizada!")
+    print("Extração das características mfcc finalizada!")
 
 
 if __name__ == "__main__":
-    save_mfcc(DATASET_PATH, JSON_PATH, num_segments=1)
+    # Descomentar as chamadas abaixo para criar todos os arquivos necessários para o projeto
+
+    # create_mfcc_features_file(DATASET_PATH, num_segments=5, track_duration=10)
+    # create_mfcc_features_file(DATASET_PATH, num_segments=5, track_duration=30)
+    # create_mfcc_features_file(DATASET_PATH, num_segments=10, track_duration=10)
+    # create_mfcc_features_file(DATASET_PATH, num_segments=10, track_duration=30)
+    # create_mfcc_features_file(DATASET_PATH, num_segments=15, track_duration=10)
+    # create_mfcc_features_file(DATASET_PATH, num_segments=15, track_duration=30)
+
+    # Comentar a chamada abaixo caso descomente as chamadas acima
+    create_mfcc_features_file(DATASET_PATH, num_segments=5, track_duration=30)
