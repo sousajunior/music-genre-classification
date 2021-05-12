23a8bbbeb46ec5ba206bb543c42f0a8b212d8e48
sousajunior <cdesousajunior@gmail.com>
2021-05-12 02:25:15 -0300

    create prediction

---
+++ extract_mfcc_of_one_music.py
@@ -0,0 +1,68 @@
+import math
+import librosa
+import numpy as np
+import warnings
+
+warnings.filterwarnings("ignore")
+
+# directory with all musics tracks
+DATASET_PATH = "genres"
+
+# default sample rate of GTZAN tracks
+SAMPLE_RATE = 22050
+
+def extract_mfcc_of_one_music(music_file_path, num_mfcc=13, n_fft=2048, hop_length=512, num_segments=10, track_duration=30):
+    """Extracts MFCCs from music dataset and saves them into a json file along witgh genre labels.
+        :param music_file_path (str): Caminho onde se encontra a música
+        :param num_mfcc (int): Número de coeficientes MFCCs para extrair
+        :param n_fft (int): Intervalo a considerar para aplicar a FFT. Medido em # de amostras
+        :param hop_length (int): Janela deslizante para FFT. Medido em # de amostras
+        :param: num_segments (int): Número de segmentos no qual cada amostra da música será dividida
+        :param: track_duration (int): Número de duração da música em segundos
+        :return inputs (ndarray): Conjunto de dados com as features extraídas da música
+        """
+
+    print("Extraindo as características da música...")
+
+    # estrutura de dados para guardar o mapping, labels e MFCCs
+    data = {
+        "mfcc": []
+    }
+
+    samples_per_track = SAMPLE_RATE * track_duration
+    samples_per_segment = int(samples_per_track / num_segments)
+    expected_num_mfcc_vectors_per_segment = math.ceil(
+        samples_per_segment / hop_length)
+
+    # carrega o arquivo de audio
+    signal, sample_rate = librosa.load(music_file_path, sr=SAMPLE_RATE)
+
+    # processa todos os segmentos dos arquivos de audio
+    for segment_index in range(num_segments):
+
+        # calcula o inicio e o fim da amostra do segmento atual
+        start_sample_index = samples_per_segment * segment_index
+        finish_sample_index = start_sample_index + samples_per_segment
+
+        # extrai o mfcc
+        mfcc = librosa.feature.mfcc(
+            signal[start_sample_index:finish_sample_index],
+            sample_rate,
+            n_mfcc=num_mfcc,
+            n_fft=n_fft,
+            hop_length=hop_length
+        )
+        mfcc = mfcc.T
+
+        # guarda apenas as características do mfcc que possui o numero esperado de vetores,
+        # pois se um mfcc não possuir a quantidade correta esperada, ele pode estragar os dados existentes
+        if len(mfcc) == expected_num_mfcc_vectors_per_segment:
+            data["mfcc"].append(mfcc.tolist())
+
+    # faz umas transformações para ajustar os dados
+    inputs = np.array(data['mfcc'])
+    inputs = inputs[..., np.newaxis]
+
+    print("Características da música extraídas com sucesso!")
+
+    return inputs[num_segments-1]
--- predict_genre.py
+++ predict_genre.py
@@ -1,16 +1,62 @@
+import os
 import numpy as np
+from extract_mfcc_of_one_music import extract_mfcc_of_one_music
+from tensorflow.keras.models import load_model
+import warnings
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+warnings.filterwarnings("ignore")
+
+genres = {0: 'blues', 1: 'clássico', 2: 'country', 3: 'discoteca',
+          4: 'hiphop', 5: 'jazz', 6: 'metal', 7: 'pop', 8: 'reggae', 9: 'rock'}
 
 
-def predict_genre(model, X, y):
-    """Prevê o gênero
-    :param model (Sequential): Modelo da rede que realizará a previsão
-    :param X (ndarray): Features da música
-    :param y (int32): Gênero da música
+def predict_genre(music_file_path, network_type='mlp'):
+    """Prevê o gênero de uma determinada música utilizando o modelo de rede escolhido
+    :param music_file_path (str): Caminho da música a ser realizada a predição
+    :param network_type (str): Tipo da rede a ser utilizada na predição (mlp, cnn, rnn)
+    :return predicted_genres_ranking (dict): Ranking com os três melhores gêneros que a rede identificou para a música selecionada
     """
+
+    # carrega a rede com o model type correspondente
+    model = load_model(f'{network_type}_genre_classifier.h5')
+    X = extract_mfcc_of_one_music(music_file_path)
 
     X = X[np.newaxis, ...]
     prediction = model.predict(X)
 
-    predicted_index = np.argmax(prediction, axis=1)
+    ranking_size = 3
+    # ranking com as posições das melhores predições, ordenadas da melhor para a pior
+    ranking = (-prediction).argsort()[0][:ranking_size]
+    predicted_genres_ranking = [dict() for x in range(ranking_size)]
 
-    print("Expected index: {}, Predicted index: {}".format(y, predicted_index))
+    # preenche um dictionary com o tamanho do ranking
+    for index in range(ranking_size):
+        # preenche o dictionary com a % de precisão na chave "accuracy"
+        predicted_genres_ranking[index]['accuracy'] = prediction[0][ranking[index]]
+        # preenche o dictionary com o nome do gênero na chave "label"
+        predicted_genres_ranking[index]['label'] = genres[ranking[index]]
+
+    return predicted_genres_ranking
+
+
+if __name__ == '__main__':
+    while(True):
+        music_file_path = input(
+            "Insira o caminho da música que você quer descobrir o gênero:")
+
+        network_type = input(
+            "Qual o tipo de RNA que você deseja usar ? (mlp, cnn, rnn):")
+
+        print('Certo, bora analisar sua música então...')
+
+        predicted_genres = predict_genre(
+            music_file_path, network_type=network_type)
+
+        print(
+            f"Os três gêneros que mais se identificam com {music_file_path} são: ")
+
+        for index, predicted_genre in enumerate(predicted_genres, start=1):
+            print("[{}] - Gênero: {}, Precisão: {:.2f}%".format(
+                index, predicted_genre['label'], predicted_genre['accuracy']))
